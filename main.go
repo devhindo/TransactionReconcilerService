da7df7bdc0b2506e0d377bd81aea66b0a6a5527f
devhindo <lildevhind@gmail.com>
2025-07-22 18:56:55 +0300

    Initialize main service structure for transaction reconciliation
    Signed-off-by: devhindo <lildevhind@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+// TransactionReconciliationService is the main service that generates the reconciliation report
+type TransactionReconciliationService struct {
+	csvReader  *CSVReader // it requires a CSVReader
+	reconciler *TransactionReconciler // a reconciler to handle the logic
+}
+
+// Constructor: NewTransactionReconciliationService creates a new service instance
+func NewTransactionReconciliationService() *TransactionReconciliationService {
+	return &TransactionReconciliationService{
+		csvReader:  NewCSVReader(),
+		reconciler: NewTransactionReconciler(),
+	}
+}
+
 func main() {
+
 	fmt.Println("Hello Extreme Solution!")
 }
